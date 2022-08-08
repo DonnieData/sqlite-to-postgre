0886c33ca9481a7c14c6b89535c0b4c9ef54e96e
donniedata <donnell.jackson@outlook.com>
2022-08-07 22:16:10 -0500

    updating queries

--- queries.sql
+++ queries.sql
@@ -16,3 +16,17 @@ ON genres.reviewid = reviews.reviewid
 WHERE Genre IS NOT null 
 GROUP BY genres.genre
 ORDER BY genres.genre DESC 
+
+
+--create view for top 10 artists by review count for each year 
+CREATE OR REPLACE VIEW ranked_artists_y AS 
+with ranked_artists AS (
+SELECT pub_year, artist, COUNT(reviewid) "Review COUNT",
+ROW_NUMBER() OVER(
+    partition by pub_year
+    order by count(reviewid) DESC ) row_num
+from reviews 
+group by pub_year, artist ) 
+select pub_year, artist, "Review Count", row_num
+from ranked_artists
+where row_num between 1 and 10 
